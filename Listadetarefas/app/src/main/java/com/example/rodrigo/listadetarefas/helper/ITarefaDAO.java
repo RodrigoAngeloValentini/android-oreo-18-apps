ad41a86fb84eb0b2ff571b33012672bc0d25e0f7
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2018-08-27 23:04:42 -0300

    lista de tarefas

---
+++ Listadetarefas/app/src/main/java/com/example/rodrigo/listadetarefas/helper/ITarefaDAO.java
@@ -0,0 +1,12 @@
+package com.example.rodrigo.listadetarefas.helper;
+
+import com.example.rodrigo.listadetarefas.model.Tarefa;
+
+import java.util.List;
+
+public interface ITarefaDAO {
+    public boolean salvar(Tarefa tarefa);
+    public boolean atualizar(Tarefa tarefa);
+    public boolean deletar(Tarefa tarefa);
+    public List<Tarefa> listar();
+}
